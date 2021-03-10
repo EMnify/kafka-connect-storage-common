ab90ede1fa06c5d1dbc7acf8541266576cbcbb94
Jiri Syrovy <jiri.syrovy@emnify.com>
2021-03-10 10:50:06 +0100

    Bumped versions, added tests

--- partitioner/src/main/java/io/confluent/connect/storage/partitioner/TimeSchemaBasedPartitioner.java
+++ partitioner/src/main/java/io/confluent/connect/storage/partitioner/TimeSchemaBasedPartitioner.java
@@ -3,7 +3,6 @@ package io.confluent.connect.storage.partitioner;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -12,16 +11,18 @@ public class TimeSchemaBasedPartitioner<T> extends TimeBasedPartitioner<T> {
 
   private static final Pattern SCHEMA_NAME = Pattern.compile("__SCHEMA_NAME__");
 
-  private Map<String, String> mappedNames = new HashMap<String, String>();
+  private final Map<String, String> mappedNames = new HashMap<String, String>();
 
   @Override
   public void configure(Map<String, Object> config) {
     super.configure(config);
-    String mappings = (String)config.getOrDefault("schema.name.mapped", "");
-    Arrays.stream(mappings.split(",")).distinct().forEach(v -> {
-      String[] pair = v.split(",");
-      mappedNames.put(pair[0], pair[1]);
-    });
+    String mappings = (String)config.getOrDefault(PartitionerConfig.SCHEMA_MAPPING_CONFIG, "");
+    for (String p : mappings.split(",")) {
+      String[] pair = p.split(":");
+      if (pair.length == 2) {
+        mappedNames.put(pair[0], pair[1]);
+      }
+    }
   }
 
 
